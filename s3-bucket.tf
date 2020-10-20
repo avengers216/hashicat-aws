093b73d278f2eb2b8e7150471b2ebc5f7a4a705d
Janardhan Vainala <janardhan.vainala@phoenix.edu>
2020-10-20 18:52:33 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/APOLLO_UOP/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  prefix = "s3-bucket"
+  bucket_prefix = "TFTW"
 }
